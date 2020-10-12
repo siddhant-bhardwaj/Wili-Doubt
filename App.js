6b4bc50e7b5b2831a36656047fbd25c09d571c9e
siddhant-bhardwaj <61611845+siddhant-bhardwaj@users.noreply.github.com>
2020-10-12 17:02:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BookTransationScreen from './screens/BookTransationScreen';
+import SearchScreen from './screens/SearchScreen';
+import {createAppContainer} from 'react-navigation'
+import {createBottomTagNavigator} from 'react-navigation-tabs';
+
+
+export default class App extends React.Component{
+  render(){
+  return (
+    <AppContainer/>
+
+  );
+  }
+}
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
+
+const TabNavigator=createBottomTabNavigator({
+  Transation:{screen:BookTransationScreen},
+  Search:{screen:SearchScreen}
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName=navigation.state.routeName
+      if(routeName===Transation){
+return(
+  <Image
+  source={require('./assests/book.png')}
+  styles={{width:40,height:40}}
+  />
+
+  
+)
+      }
+
+
+else if(routeName===Search){
+return(
+  <Image
+  source={require('./assests/booklogo.png')}
+  styles={{width:40,height:40}}
+  />
+)
+}
+
+    }
+  })
+}
+);
+
+const AppContainer=createAppContainer(TabNavigator);
